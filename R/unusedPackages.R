05229c75375742e38e4c71211b0a0d72878641bc
wessel-mulder <140071993+wessel-mulder@users.noreply.github.com>
2025-04-15 11:19:02 +0200

    Update unusedPackages.R

--- R/unusedPackages.R
+++ R/unusedPackages.R
@@ -4,18 +4,34 @@
 #' @importFrom funspotr spot_funs
 #'
 
-unusedPackages <- function(pkgs){
-  loadInstall(pkgs)
-  file_path <- currentScript()
-  funs <- funspotr::spot_funs(file_path)
-  diff <- setdiff(pkgs, unique(funs$pkgs))
+#' Detect unused packages without explicit pkg::fun() calls
+#'
+#' @param pkgs A character vector of packages you want to check.
+#' @param file_path The path to the R script.
+#' @return A vector of unused packages.
+unusedPackages <- function(pkgs, file_path) {
+  code <- parse(file_path)
+  funs <- codetools::findGlobals(code, merge = FALSE)$functions
 
-  if (length(diff) == 0) {
+  find_function_origin <- function(fn) {
+    env <- find(fn, mode = "function")
+    if (length(env) == 0) return(NA)
+    env <- env[1]
+    if (startsWith(env, "package:")) {
+      return(sub("package:", "", env))
+    } else {
+      return(NA)
+    }
+  }
+
+  pkgs_used <- unique(na.omit(sapply(funs, find_function_origin)))
+  unused <- setdiff(pkgs, pkgs_used)
+
+  if (length(unused) == 0) {
     message("No unused packages.")
     return(invisible(NULL))
   } else {
-    message(paste0( length(diff), " unused package(s):"))
-    return(diff)
+    message(paste0(length(unused), " unused package(s):"))
+    return(unused)
   }
 }
-
