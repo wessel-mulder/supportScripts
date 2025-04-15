ea33b96a8a35ab38d0a21fc84c6eb99e706dd7d5
wessel-mulder <wesselmulder00@gmail.com>
2025-04-15 10:53:41 +0200

    load libraries first

--- R/unusedPackages.R
+++ R/unusedPackages.R
@@ -3,6 +3,7 @@
 #' @export
 #' @importFrom funspotr spot_funs
 unusedPackages <- function(pkgs){
+  loadInstall(pkgs)
   funs <- funspotr::spot_funs(rstudioapi::getActiveDocumentContext()$path)
   diff <- setdiff(pkgs,unique(funs$pkgs))
   if(length(diff) == 0){
